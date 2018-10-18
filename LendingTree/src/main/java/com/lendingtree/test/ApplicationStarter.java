251b362f8dfc7152b8a366a8bdbc14f40d84599f
romana <romana.firdouse@gmail.com>
2018-10-18 16:43:32 -0500

    LendingTree Week1

---
+++ LendingTree/src/main/java/com/lendingtree/test/ApplicationStarter.java
@@ -0,0 +1,11 @@
+package com.lendingtree.test;
+
+public class ApplicationStarter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ApplicationStarter.main()");
+		System.out.println("Welcome to Lending Tree---------Apps");
+	}
+
+}
